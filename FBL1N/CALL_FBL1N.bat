00cf390b46bca34e92b402c4623206c249180d23
iuribiasi <iuribiasi1@gmail.com>
2022-07-23 10:40:22 -0300

    python_sap_aut

---
+++ FBL1N/CALL_FBL1N.bat
@@ -0,0 +1,3 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+call "C:\Users\I7770871\OneDrive - Saint-Gobain\Pessoal - Iuri\Automatizacao_Python\SAP\FBL1N\FBL1N.bat"
+pause
---
+++ FBL1N/FBL1N.bat
@@ -0,0 +1,2 @@
+"C:\ProgramData\Anaconda3\python.exe" "C:\Users\I7770871\OneDrive - Saint-Gobain\Pessoal - Iuri\Automatizacao_Python\SAP\FBL1N\FBL1N.py"
+pause
